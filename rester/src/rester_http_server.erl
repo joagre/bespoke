42a40fbab612f40411cd5c1fc03337262b838cd6
Joakim G. <jocke@gmail.com>
2024-11-12 16:57:16 +0100

    being anal

--- rester/src/rester_http_server.erl
+++ rester/src/rester_http_server.erl
@@ -132,7 +132,8 @@ stop(Pid) ->
 
 init(Socket, Options) ->
     ?debug("connection on: ~p ", [Socket]),
-    {ok, _PeerName} = rester_socket:peername(Socket),
+    %%{ok, _PeerName} = rester_socket:peername(Socket),
+    _PeerName = undefined,
     {ok, _SockName} = rester_socket:sockname(Socket),
     ?debug("connection from peer: ~p, sockname: ~p,"
 		"options ~p", [_PeerName, _SockName, Options]),
