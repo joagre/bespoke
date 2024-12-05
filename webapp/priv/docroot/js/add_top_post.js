8a9fd349942e49286b54abbb0acfa9f473750c56
Joakim G. <jocke@gmail.com>
2024-12-05 23:00:41 +0100

    being anal

--- webapp/priv/docroot/js/add_top_post.js
+++ webapp/priv/docroot/js/add_top_post.js
@@ -13,6 +13,8 @@ class AddTopPost {
     this._formFields = Array.from(
       document.querySelectorAll("#form-title, #form-body")
     );
+    this._formTitle = document.getElementById("form-title");
+    this._formTitle.focus();
     this._addButton = document.getElementById("add-button");
     this._attachEventListeners();
     this._updatePage();
@@ -32,27 +34,25 @@ class AddTopPost {
   }
 
   _updatePage() {
-    // Insert username into title
-    let username = bespoke.getCookieValue("username");
+    const username = bespoke.getCookieValue("username");
     document.getElementById("title-username").textContent = username;
   }
 
   addTopPost(event) {
     event.preventDefault();
-    const post = {
-      title: document.getElementById("form-title").value,
-      body: document.getElementById("form-body").value,
-    };
-
     const updateServer = async () => {
       try {
+        const payload = {
+          title: document.getElementById("form-title").value,
+          body: document.getElementById("form-body").value,
+        };
         // REST: Add top post
         const response = await fetch("/insert_post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(post),
+          body: JSON.stringify(payload),
         });
         if (!response.ok) {
           if (response.status === 403) {
@@ -67,7 +67,6 @@ class AddTopPost {
         console.error("Addition of top post failed:", error);
       }
     };
-
     updateServer();
   }
 }
