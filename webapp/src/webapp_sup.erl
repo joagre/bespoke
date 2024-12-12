928511ca534201865e6f4175eb8d30fd6fa52dc7
Joakim G. <jocke@gmail.com>
2024-12-12 23:57:46 +0100

    forgot modules!

---
+++ webapp/src/webapp_sup.erl
@@ -0,0 +1,27 @@
+-module(webapp_sup).
+-behaviour(supervisor).
+-export([start_link/0]).
+-export([init/1]). %% Used by supervisor:start_link/2
+
+%%
+%% Exported: start_link
+%%
+
+-spec start_link() -> any().
+
+start_link() ->
+    case supervisor:start_link(?MODULE, []) of
+        {ok, SupervisorPid} ->
+            {ok, SupervisorPid};
+        Error ->
+            Error
+    end.
+
+%%
+%% Exported: init
+%%
+
+init([]) ->
+    WebappRestSpec =
+        #{id => webapp_rest, start => {webapp_rest, start_link, []}},
+    {ok, {#{strategy => one_for_all}, [WebappRestSpec]}}.
