6b6afefdea71063527f63e83305f18a5fd4e5201
Joakim G. <jocke@gmail.com>
2024-12-15 20:01:13 +0100

    FOCKmake!

--- apptools/include/log.hrl
+++ apptools/include/log.hrl
@@ -1,34 +1,29 @@
 -ifndef(LOG_HRL).
 -define(LOG_HRL, true).
 
--define(log_info(Format),
-        logger:info(Format, [], #{module => ?MODULE,
-                                  line => ?LINE,
-                                  function => ?FUNCTION_NAME})).
-
--define(log_info(Format, Args),
-        logger:info(Format, Args, #{module => ?MODULE,
-                                    line => ?LINE,
-                                    function => ?FUNCTION_NAME})).
 
 -define(log_debug(Format),
-        logger:debug(Format, [], #{module => ?MODULE,
-                                   line => ?LINE,
-                                   function => ?FUNCTION_NAME})).
+        io:format("**** DEBUG ~w:~w:~w:" ++ Format ++ "\n",
+                  [?MODULE, ?LINE, ?FUNCTION_NAME])).
 
 -define(log_debug(Format, Args),
-        logger:debug(Format, Args, #{module => ?MODULE,
-                                     line => ?LINE,
-                                     function => ?FUNCTION_NAME})).
+        io:format("**** DEBUG ~w:~w:~w:" ++ Format ++ "\n",
+                  [?MODULE, ?LINE, ?FUNCTION_NAME|Args])).
+
+-define(log_info(Format),
+        io:format("**** INFO ~w:~w:~w:" ++ Format ++ "\n",
+                  [?MODULE, ?LINE, ?FUNCTION_NAME])).
+
+-define(log_info(Format, Args),
+        io:format("**** INFO ~w:~w:~w:" ++ Format ++ "\n",
+                  [?MODULE, ?LINE, ?FUNCTION_NAME|Args])).
 
 -define(log_error(Format),
-        logger:error(Format, [], #{module => ?MODULE,
-                                   line => ?LINE,
-                                   function => ?FUNCTION_NAME})).
+        io:format("**** ERROR ~w:~w:~w:" ++ Format ++ "\n",
+                  [?MODULE, ?LINE, ?FUNCTION_NAME])).
 
 -define(log_error(Format, Args),
-        logger:error(Format, Args, #{module => ?MODULE,
-                                     line => ?LINE,
-                                     function => ?FUNCTION_NAME})).
+        io:format("**** ERROR ~w:~w:~w:" ++ Format ++ "\n",
+                  [?MODULE, ?LINE, ?FUNCTION_NAME|Args])).
 
 -endif.
