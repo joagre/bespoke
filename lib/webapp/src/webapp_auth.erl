834b37af7a95383c2611f0c5f865d924e915b007
Joakim G. <jocke@gmail.com>
2024-12-25 01:43:52 +0100

    Removing the need for libsodium on the server

---
+++ lib/webapp/src/webapp_auth.erl
@@ -0,0 +1,28 @@
+-module(webapp_auth).
+-export([generate_challenge/0, verify_client_response/3]).
+-export_type([challenge/0, client_response/0]).
+
+-define(CHALLENGE_SIZE, 32).
+
+-type challenge() :: binary().
+-type client_response() :: binary().
+
+%%
+%% Exported: generate_challenge
+%%
+
+-spec generate_challenge() -> challenge().
+
+generate_challenge() ->
+    crypto:strong_rand_bytes(?CHALLENGE_SIZE).
+
+%%
+%% Exported: verify_client_response
+%%
+
+-spec verify_client_response(
+        client_response(), challenge(), db_user_serv:pwhash()) -> boolean().
+
+verify_client_response(ClientResponse, Challenge, Pwhash) ->
+    ExpectedResponse = crypto:mac(hmac, sha256, Pwhash, Challenge),
+    crypto:hash_equals(ExpectedResponse, ClientResponse).
