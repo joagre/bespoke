59b13d50d54ab149503aa8cac70e0d159aa7c57d
Joakim G. <jocke@gmail.com>
2025-01-31 21:34:03 +0100

    being anal

--- bin/purge-android-mini-browser.sh
+++ bin/purge-android-mini-browser.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 adb shell pm clear com.google.android.webview
 adb shell pm clear com.android.chrome
+adb shell pm uninstall --user 0 com.google.android.webview
+adb shell cmd package install-existing com.google.android.webview
