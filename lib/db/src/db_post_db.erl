5239d2912a35a4765634bfb272609e4515de5bc6
Joakim G. <joagre@gmail.com>
2025-03-03 20:32:54 +0100

    refactoring deluxe

---
+++ lib/db/src/db_post_db.erl
@@ -0,0 +1,256 @@
+% -*- fill-column: 100; -*-
+
+-module(db_post_db).
+-export([open/0, dump/0, sync/0, close/0, create_post/1, read_top_posts/0, read_posts/2,
+         read_post_ids/2, delete_post/1, toggle_post_like/2]).
+
+-include_lib("apptools/include/shorthand.hrl").
+-include_lib("apptools/include/log.hrl").
+-include("../include/db.hrl").
+
+-define(POST_FILE_PATH, filename:join(?BESPOKE_DB_DIR, "post.db")).
+-define(POST_DB, post_db).
+
+%%
+%% Exported: open
+%%
+
+-spec open() -> ok.
+
+open() ->
+    {ok, _} = dets:open_file(?POST_DB, [{file, ?POST_FILE_PATH}, {keypos, #post.id}]),
+    ok.
+
+%%
+%% Exported: dump
+%%
+
+-spec dump() -> [{dets:tab_name(), [#post{}]}].
+
+dump() ->
+    [{?POST_DB, db:dets_dump(?POST_DB)}].
+
+%%
+%% Exported: sync
+%%
+
+-spec sync() -> ok | {error, term()}.
+
+sync() ->
+    dets:sync(?POST_DB).
+
+%%
+%% Exported: close
+%%
+
+-spec close() -> ok | {error, term()}.
+
+close() ->
+    dets:close(?POST_DB).
+
+%%
+%% Exported: create_post
+%%
+
+-spec create_post(#post{}) -> {ok, #post{}} | {error, invalid_post}.
+
+create_post(Post) ->
+    case is_valid_post(Post) of
+        {true, ParentPost, _TopPost}  ->
+            NewPostId =
+                case Post#post.id of
+                    not_set ->
+                        NextPostId = db_meta_db:read_next_post_id(),
+                        ?i2b(NextPostId);
+                    PostId ->
+                        PostId
+                end,
+            UpdatedAttachments = move_tmp_attachments(Post#post.attachments, NewPostId),
+            UpdatedPost = Post#post{id = NewPostId,
+                                    created = db:seconds_since_epoch(Post#post.created),
+                                    attachments = UpdatedAttachments},
+            ok = insert_and_inform(UpdatedPost),
+            case ParentPost of
+                not_set ->
+                    {ok, UpdatedPost};
+                #post{replies = Replies} ->
+                    UpdatedParentPost = ParentPost#post{replies = Replies ++ [NewPostId]},
+                    ok = insert_and_inform(UpdatedParentPost),
+                    ok = update_parent_count(ParentPost#post.id, 1),
+                    {ok, UpdatedPost}
+            end;
+        false ->
+            {error, invalid_post}
+    end.
+
+is_valid_post(#post{id = not_set} = Post) ->
+    check_post(Post);
+is_valid_post(Post) ->
+    case dets:lookup(?POST_DB, Post#post.id) of
+        [] ->
+            check_post(Post);
+        _ ->
+            false
+    end.
+
+check_post(#post{title = Title,
+                 parent_post_id = not_set,
+                 top_post_id = not_set,
+                 created = Created})
+  when Title /= not_set andalso
+       (Created == not_set orelse is_integer(Created)) ->
+    {true, not_set, not_set};
+check_post(#post{title = not_set,
+                 parent_post_id = ParentPostId,
+                 top_post_id = TopPostId,
+                 created = Created})
+  when ParentPostId /= not_set andalso
+       TopPostId /= not_set andalso
+       (Created == not_set orelse is_integer(Created)) ->
+    case dets:lookup(?POST_DB, ParentPostId) of
+        [ParentPost] ->
+            case dets:lookup(?POST_DB, TopPostId) of
+                [TopPost] ->
+                    {true, ParentPost, TopPost};
+                [] ->
+                    false
+            end;
+        [] ->
+            false
+    end;
+check_post(_) ->
+    false.
+
+move_tmp_attachments(TmpAttachments, NewPostId) ->
+    NewPath = filename:join([?BESPOKE_ATTACHMENT_PATH, NewPostId]),
+    ok = file:make_dir(NewPath),
+    TmpPath = ?BESPOKE_TMP_PATH,
+    lists:map(
+      fun({TmpAttachment, ContentType}) ->
+              TmpFilePath = filename:join([TmpPath, TmpAttachment]),
+              NewAttachment = string:trim(TmpAttachment, leading, "0123456789-"),
+              NewFilePath = filename:join([NewPath, NewAttachment]),
+              ?log_info("Moving ~s to ~s", [TmpFilePath, NewFilePath]),
+              ok = file:rename(TmpFilePath, NewFilePath),
+              {NewAttachment, ContentType}
+      end, TmpAttachments).
+
+%%
+%% Exported: read_top_posts
+%%
+
+-spec read_top_posts() -> [#post{}].
+
+read_top_posts() ->
+    sort_posts(dets:match_object(?POST_DB, #post{top_post_id = not_set, _ = '_'})).
+
+%%
+%% Exported: read_posts
+%%
+
+-spec read_posts([db:post_id()], flat | recursive) -> [#post{}].
+
+read_posts(PostIds, Mode) ->
+    Posts =
+        lists:foldr(
+          fun(PostId, Acc) ->
+                  case dets:lookup(?POST_DB, PostId) of
+                      [Post] when Mode == flat ->
+                          [Post|Acc];
+                      [Post] when Mode == recursive ->
+                          Replies = read_posts(Post#post.replies, Mode),
+                          [Post|Acc] ++ Replies
+                  end
+          end, [], PostIds),
+    sort_posts(Posts).
+
+%%
+%% Exported: read_post_ids
+%%
+
+-spec read_post_ids([db:post_id()], flat | recursive) -> [db:post_id()].
+
+read_post_ids(PostIds, Mode) ->
+    lists:foldr(fun(PostId, Acc) ->
+                        case dets:lookup(?POST_DB, PostId) of
+                            [Post] when Mode == flat ->
+                                [Post#post.id|Acc];
+                            [Post] when Mode == recursive ->
+                                Replies = read_post_ids(Post#post.replies, Mode),
+                                [Post#post.id|Acc] ++ Replies
+                        end
+                end, [], PostIds).
+
+%%
+%% Exported: delete_post
+%%
+
+-spec delete_post(db:post_id()) -> ok | {error, not_found}.
+
+delete_post(PostId) ->
+    case dets:lookup(?POST_DB, PostId) of
+        [#post{parent_post_id = ParentPostId}] when ParentPostId /= not_set ->
+            [ParentPost] = dets:lookup(?POST_DB, ParentPostId),
+            ok = insert_and_inform(
+                   ParentPost#post{replies = lists:delete(PostId, ParentPost#post.replies)}),
+            N = delete_all([PostId]),
+            update_parent_count(ParentPost#post.id, -N);
+        [_TopPost] ->
+            _N = delete_all([PostId]),
+            ok;
+        [] ->
+            {error, not_found}
+    end.
+
+delete_all([]) ->
+    0;
+delete_all([PostId|Rest]) ->
+    [Post] = dets:lookup(?POST_DB, PostId),
+    ok = delete_and_inform(PostId),
+    delete_all(Post#post.replies) + delete_all(Rest) + 1.
+
+%%
+%% Exported: toggle_post_like
+%%
+
+-spec toggle_post_like(db:post_id(), db:user_id()) ->
+          {ok, [db:user_id()]} | {error, not_found}.
+
+toggle_post_like(PostId, UserId) ->
+    case dets:lookup(?POST_DB, PostId) of
+        [#post{likers = Likers} = Post] ->
+            UpdatedLikers = case lists:member(UserId, Likers) of
+                                true ->
+                                    lists:delete(UserId, Likers);
+                                false ->
+                                    [UserId|Likers]
+                            end,
+            ok = insert_and_inform(Post#post{likers = UpdatedLikers}),
+            {ok, UpdatedLikers};
+        [] ->
+            {error, not_found}
+    end.
+
+%%
+%% Utilities
+%%
+
+sort_posts(Posts) ->
+    lists:sort(fun(PostA, PostB) ->
+                       PostA#post.created =< PostB#post.created
+               end, Posts).
+
+delete_and_inform(PostId) ->
+    ok = dets:delete(?POST_DB, PostId),
+    db_subscription_db:inform_subscribers(PostId).
+
+insert_and_inform(Post) when is_record(Post, post) ->
+    ok = dets:insert(?POST_DB, Post),
+    db_subscription_db:inform_subscribers(Post#post.id).
+
+update_parent_count(not_set, _N) ->
+    ok;
+update_parent_count(PostId, N) ->
+    [Post] = dets:lookup(?POST_DB, PostId),
+    ok = insert_and_inform(Post#post{reply_count = Post#post.reply_count + N}),
+    update_parent_count(Post#post.parent_post_id, N).
--- lib/db/src/db_serv.erl
+++ lib/db/src/db_serv.erl
@@ -16,11 +16,16 @@
 -include_lib("apptools/include/serv.hrl").
 -include("../include/db.hrl").
 
-%% Post DB
+-record(state, {parent :: pid()}).
+
+
+%% REMOVE
 -define(POST_FILE_PATH, filename:join(?BESPOKE_DB_DIR, "post.db")).
 -define(POST_DB, post_db).
 
--record(state, {parent :: pid()}).
+
+
+
 
 %%
 %% Exported: start_link
@@ -135,7 +140,7 @@ read_post_ids(PostIds, Mode) ->
     serv:call(?MODULE, {read_post_ids, PostIds, Mode}).
 
 %%
-%% delete_post
+%% Exported: delete_post
 %%
 
 -spec delete_post(db:post_id()) -> ok | {error, not_found}.
@@ -252,52 +257,22 @@ message_handler(S) ->
         %% Forum
         {call, From, {create_post, Post} = Call} ->
             ?log_debug("Call: ~p", [Call]),
-            {reply, From, do_insert_post(Post)};
+            {reply, From, db_post_db:create_post(Post)};
         {call, From, read_top_posts = Call} ->
             ?log_debug("Call: ~p", [Call]),
-            TopPosts = dets:match_object(?POST_DB, #post{top_post_id = not_set, _ = '_'}),
-            SortedTopPosts = lists:sort(fun(PostA, PostB) ->
-                                                PostA#post.created > PostB#post.created
-                                        end, TopPosts),
-            {reply, From, SortedTopPosts};
+            {reply, From, db_post_db:read_top_posts()};
         {call, From, {read_posts, PostIds, Mode} = Call} ->
             ?log_debug("Call: ~p", [Call]),
-            {reply, From, do_lookup_posts(PostIds, Mode)};
+            {reply, From, db_post_db:read_posts(PostIds, Mode)};
         {call, From, {read_post_ids, PostIds, Mode} = Call} ->
             ?log_debug("Call: ~p", [Call]),
-            {reply, From, do_lookup_post_ids(PostIds, Mode)};
+            {reply, From, read_post_ids(PostIds, Mode)};
         {call, From, {delete_post, PostId} = Call} ->
             ?log_debug("Call: ~p", [Call]),
-            case dets:lookup(?POST_DB, PostId) of
-                [#post{parent_post_id = ParentPostId}] when ParentPostId /= not_set ->
-                    [ParentPost] = dets:lookup(?POST_DB, ParentPostId),
-                    ok = insert_and_inform(
-                           ParentPost#post{
-                             replies = lists:delete(PostId, ParentPost#post.replies)}),
-                    N = delete_all([PostId]),
-                    ok = update_parent_count(ParentPost#post.id, -N),
-                    {reply, From, ok};
-                [_TopPost] ->
-                    _N = delete_all([PostId]),
-                    {reply, From, ok};
-                [] ->
-                    {reply, From, {error, not_found}}
-            end;
+            {reply, From, db_post_db:delete_post(PostId)};
         {call, From, {toggle_post_like, PostId, UserId} = Call} ->
             ?log_debug("Call: ~p", [Call]),
-            case dets:lookup(?POST_DB, PostId) of
-                [#post{likers = Likers} = Post] ->
-                    UpdatedLikers = case lists:member(UserId, Likers) of
-                                        true ->
-                                            lists:delete(UserId, Likers);
-                                        false ->
-                                            [UserId|Likers]
-                                    end,
-                    ok = insert_and_inform(Post#post{likers = UpdatedLikers}),
-                    {reply, From, {ok, UpdatedLikers}};
-                [] ->
-                    {reply, From, {error, not_found}}
-            end;
+            {reply, From, db_post_db:toggle_post_like(PostId, UserId)};
         %% File sharing
         {call, From, {create_file, File} = Call} ->
             ?log_debug("Call: ~p", [Call]),
@@ -314,7 +289,7 @@ message_handler(S) ->
         {call, From, {file_is_uploaded, FileId} = Call} ->
             ?log_debug("Call: ~p", [Call]),
             {reply, From, db_file_db:file_is_uploaded(FileId)};
-        %% Subscription handling
+        %% Subscription management
         {call, From, {subscribe_on_changes, Subscriber, PostIds} = Call} ->
             ?log_debug("Call: ~p", [Call]),
             {reply, From, db_subscription_db:subscribe(Subscriber, PostIds)};
@@ -325,9 +300,8 @@ message_handler(S) ->
         %% Database management
         {call, From, sync = Call} ->
             ?log_debug("Call: ~p", [Call]),
-            ok = sync_dbs(),
-            {reply, From, ok};
-        %% System management
+            {reply, From, sync_dbs()};
+        %% System messages
         {'EXIT', Pid, Reason} when S#state.parent == Pid ->
             ok = close_dbs(),
             exit(Reason);
@@ -342,169 +316,20 @@ message_handler(S) ->
 open_dbs() ->
     ok = db_meta_db:open(),
     ok = db_message_db:open(),
-    {ok, _} = dets:open_file(?POST_DB, [{file, ?POST_FILE_PATH}, {keypos, #post.id}]),
+    ok = db_post_db:open(),
     ok = db_file_db:open(),
     ok = db_subscription_db:open().
 
 close_dbs() ->
     _ = db_subscription_db:close(),
     _ = db_file_db:close(),
-    _ = dets:close(?POST_DB),
+    _ = db_post_db:close(),
     _ = db_message_db:close(),
     _ = db_meta_db:close(),
     ok.
 
 sync_dbs() ->
     ok = db_file_db:sync(),
-    ok = dets:sync(?POST_DB),
+    ok = db_post_db:sync(),
     ok = db_message_db:sync(),
     db_meta_db:sync().
-
-%%
-%% Lookup posts
-%%
-
-do_lookup_posts(PostIds, Mode) ->
-    Posts =
-        lists:foldr(
-          fun(PostId, Acc) ->
-                  case dets:lookup(?POST_DB, PostId) of
-                      [Post] when Mode == flat ->
-                          [Post|Acc];
-                      [Post] when Mode == recursive ->
-                          Replies = do_lookup_posts(Post#post.replies, Mode),
-                          [Post|Acc] ++ Replies
-                  end
-          end, [], PostIds),
-    lists:sort(
-      fun(PostA, PostB) ->
-              PostA#post.created =< PostB#post.created
-      end, Posts).
-
-do_lookup_post_ids(PostIds, Mode) ->
-    lists:foldr(
-      fun(PostId, Acc) ->
-              case dets:lookup(?POST_DB, PostId) of
-                  [Post] when Mode == flat ->
-                      [Post#post.id|Acc];
-                  [Post] when Mode == recursive ->
-                      Replies = do_lookup_post_ids(Post#post.replies, Mode),
-                      [Post#post.id|Acc] ++ Replies
-              end
-      end, [], PostIds).
-
-%%
-%% Insert post
-%%
-
-do_insert_post(Post) ->
-    case is_valid_insert_post(Post) of
-        {true, ParentPost, _TopPost}  ->
-            NewPostId =
-                case Post#post.id of
-                    not_set ->
-                        NextPostId = db_meta_db:read_next_post_id(),
-                        ?i2b(NextPostId);
-                    PostId ->
-                        PostId
-                end,
-            UpdatedAttachments = move_tmp_attachments(Post#post.attachments, NewPostId),
-            UpdatedPost = Post#post{
-                            id = NewPostId,
-                            created = db:seconds_since_epoch(Post#post.created),
-                            attachments = UpdatedAttachments},
-            ok = insert_and_inform(UpdatedPost),
-            case ParentPost of
-                not_set ->
-                    {ok, UpdatedPost};
-                #post{replies = Replies} ->
-                    UpdatedParentPost = ParentPost#post{replies = Replies ++ [NewPostId]},
-                    ok = insert_and_inform(UpdatedParentPost),
-                    ok = update_parent_count(ParentPost#post.id, 1),
-                    {ok, UpdatedPost}
-            end;
-        false ->
-            {error, invalid_post}
-    end.
-
-is_valid_insert_post(#post{id = not_set} = Post) ->
-    check_insert_post(Post);
-is_valid_insert_post(Post) ->
-    case dets:lookup(?POST_DB, Post#post.id) of
-        [] ->
-            check_insert_post(Post);
-        _ ->
-            false
-    end.
-
-check_insert_post(#post{title = Title,
-                        parent_post_id = not_set,
-                        top_post_id = not_set,
-                        created = Created})
-  when Title /= not_set andalso
-       (Created == not_set orelse is_integer(Created)) ->
-    {true, not_set, not_set};
-check_insert_post(#post{title = not_set,
-                        parent_post_id = ParentPostId,
-                        top_post_id = TopPostId,
-                        created = Created})
-  when ParentPostId /= not_set andalso
-       TopPostId /= not_set andalso
-       (Created == not_set orelse is_integer(Created)) ->
-    case dets:lookup(?POST_DB, ParentPostId) of
-        [ParentPost] ->
-            case dets:lookup(?POST_DB, TopPostId) of
-                [TopPost] ->
-                    {true, ParentPost, TopPost};
-                [] ->
-                    false
-            end;
-        [] ->
-            false
-    end;
-check_insert_post(_) ->
-    false.
-
-move_tmp_attachments(TmpAttachments, NewPostId) ->
-    NewPath = filename:join([?BESPOKE_ATTACHMENT_PATH, NewPostId]),
-    ok = file:make_dir(NewPath),
-    TmpPath = ?BESPOKE_TMP_PATH,
-    lists:map(
-      fun({TmpAttachment, ContentType}) ->
-              TmpFilePath = filename:join([TmpPath, TmpAttachment]),
-              NewAttachment = string:trim(TmpAttachment, leading, "0123456789-"),
-              NewFilePath = filename:join([NewPath, NewAttachment]),
-              ?log_info("Moving ~s to ~s", [TmpFilePath, NewFilePath]),
-              ok = file:rename(TmpFilePath, NewFilePath),
-              {NewAttachment, ContentType}
-      end, TmpAttachments).
-
-update_parent_count(not_set, _N) ->
-    ok;
-update_parent_count(PostId, N) ->
-    [Post] = dets:lookup(?POST_DB, PostId),
-    ok = insert_and_inform(Post#post{reply_count = Post#post.reply_count + N}),
-    update_parent_count(Post#post.parent_post_id, N).
-
-%%
-%% Delete all posts (recursively)
-%%
-
-delete_all([]) ->
-    0;
-delete_all([PostId|Rest]) ->
-    [Post] = dets:lookup(?POST_DB, PostId),
-    ok = delete_and_inform(PostId),
-    delete_all(Post#post.replies) + delete_all(Rest) + 1.
-
-%%
-%% Subscription handling
-%%
-
-insert_and_inform(Post) when is_record(Post, post) ->
-    ok = dets:insert(?POST_DB, Post),
-    db_subscription_db:inform_subscribers(Post#post.id).
-
-delete_and_inform(PostId) ->
-    ok = dets:delete(?POST_DB, PostId),
-    db_subscription_db:inform_subscribers(PostId).
